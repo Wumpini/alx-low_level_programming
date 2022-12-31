f379b3f136a1add197e12a256e90688413702cbd
hamdan <hamdanak49@gmail.com>
2022-12-30 23:09:34 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,10 +5,10 @@
  * @t: the integer to be computed
  * Return: the absolute value
  */
-int _abs(int r)
+int _abs(int t)
 {
-	if (r >= 0)
-		return (r);
+	if (t >= 0)
+		return (t);
 	else
-		return (_r);
+		return (-t);
 }
