8aa53840fe526bdc4e9de673d1bc9ae71b66770b
hamdan <hamdanak49@gmail.com>
2022-12-31 00:11:41 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,19 +3,17 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @: the number to be treated
- * Return: value of the last digit of number 
+ * Return: value of the last digit of number
  */
 int print_last_digit(int n)
 {
 	int last;
 
 	last = n % 10;
-
 	if (last < 0)
 	{
 	last = last * -1;
 	}
-
 	_putchar(last + '0');
 	return (last);
 }
